cefc6adbe3124fc9a892c514a1a3b6b0944141a2
Birat Panta <pantabirat@gmail.com>
2021-02-22 19:47:11 +0545

    Add project files.

---
+++ Areas/Identity/Data/DemoWebAppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace DemoWebApp.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the DemoWebAppUser class
+    public class DemoWebAppUser : IdentityUser
+    {
+        [Column(TypeName ="varchar(50)")]
+        public string UserType { get; set; }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using DemoWebApp.Areas.Identity.Data;
+using DemoWebApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DemoWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace DemoWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DemoWebAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DemoWebAppContextConnection")));
+
+                services.AddDefaultIdentity<DemoWebAppUser>(
+
+                    options => { 
+                        options.SignIn.RequireConfirmedAccount = false;
+                        options.Password.RequireLowercase = false;
+                        options.Password.RequireUppercase = false;
+                        options.Password.RequireNonAlphanumeric = false;
+                        options.Password.RequiredLength = 1;
+                        options.Password.RequireDigit = false;
+
+                    })
+                    .AddEntityFrameworkStores<DemoWebAppContext>();
+            });
+        }
+    }
+}
